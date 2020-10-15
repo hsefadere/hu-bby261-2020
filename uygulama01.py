01a65103fc98f0dc15e77857de6e3de141ea9b18
Harun Sefa DERE <48154854+hsefadere@users.noreply.github.com>
2020-10-15 23:50:11 +0300

    Update and rename uygulama01 to uygulama01.py

---
+++ uygulama01.py
@@ -0,0 +1,5 @@
+
+metin = "bilgi insanlar için bir ayrıcalık olmakla birlikte bilgiler sayesinde geleceğin aydınlanacağı bilinmektedir."
+kelime = metin.split()
+sonmetin = kelime.count("bilgi")
+print(sonmetin)
